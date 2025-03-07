717599866f8325e3994643d3ec63e1a9013c1093
MisaghM <misagh.mhgg@gmail.com>
2025-03-07 04:58:13 +0330

    Small cleanup

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse, HTMLResponse
 import json
 from pathlib import Path
 
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse, HTMLResponse
+
+
+DATA_DIR = Path("./assets/data/")
+JSON_FILES = ["users.json", "authors.json", "books.json", "reviews.json"]
+
 app = FastAPI()
 
-# Directory where JSON files are stored
-DATA_DIR = Path("./assets/data/")
 
-# JSON file names
-JSON_FILES = ["users.json", "reviews.json", "authors.json", "books.json"]
-
-
-def read_json(file_name):
+def read_json(file_name: str) -> dict:
     file_path = DATA_DIR / file_name
     if file_path.exists():
         with open(file_path, "r", encoding="utf-8") as f:
@@ -21,15 +20,20 @@ def read_json(file_name):
 
 
 @app.get("/", response_class=HTMLResponse)
-def home():
+def index():
     links = "".join([f'<li><a href="/{file}">{file}</a></li>' for file in JSON_FILES])
     html_content = f"""
-    <html>
-        <head><title>JSON File Server</title></head>
-        <body>
-            <h1>Available JSON Files</h1>
-            <ul>{links}</ul>
-        </body>
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>IE API</title>
+      </head>
+      <body>
+        <h1>IE API (Spring 04)</h1>
+        <h2>Endpoints:</h2>
+        <ul>{links}</ul>
+      </body>
     </html>
     """
     return HTMLResponse(content=html_content)
@@ -47,5 +51,4 @@ def get_json(file_name: str):
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
